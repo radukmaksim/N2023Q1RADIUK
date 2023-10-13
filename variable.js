e0cd008b07ab3aef7e485dc353fc8eb780d4bd71
Raduk Maksim <maksimraduk8@gmail.com>
2023-10-13 11:32:46 +0300

    add: variable.js

---
+++ variable.js
@@ -0,0 +1,8 @@
+const fnumber = 7123
+const snumber = 8
+const result = fnumber + snumber
+const fword = "Привет "
+const secword = "мир"
+const concatTwoWord = fword + secword
+console.log(result)
+console.log(concatTwoWord)
