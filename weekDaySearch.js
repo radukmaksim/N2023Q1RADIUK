bb00a125cb3c43ef9665d5cfd79e0d24ac35bbf3
Raduk Maksim <maksimraduk8@gmail.com>
2023-10-27 11:01:12 +0300

    add: search  day

---
+++ weekDaySearch.js
@@ -0,0 +1,18 @@
+const numericRepresentationOfTheDayOfTheWeek = 66
+
+if (numericRepresentationOfTheDayOfTheWeek === 1)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Понедельник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 2)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Вторник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 3)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Среда`)
+else if (numericRepresentationOfTheDayOfTheWeek === 4)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Четверг`)
+else if (numericRepresentationOfTheDayOfTheWeek === 5)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Пятница`)
+else if (numericRepresentationOfTheDayOfTheWeek === 6)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Суббота`)
+else if (numericRepresentationOfTheDayOfTheWeek === 7)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - это Воскресенье`)
+else console.log('неверные данные') 
+
